f2a7ed4efd608c0e99343ce9c8caf6441a896037
Mihael Lenic <mihael.lenic@gmail.com>
2022-11-02 15:13:04 +0100

    upload and parse csv

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import { ChartComponent } from "src/components/chart/chart.component";
 import { Button, CssVarsProvider } from "@mui/joy";
+import { ChangeEvent } from "react";
+import * as Papa from "papaparse";
 
 function App() {
+  function onUploadChange(e: ChangeEvent<HTMLInputElement>) {
+    const { files } = e.target;
+
+    if (files && files.length > 0) {
+      Papa.parse(files[0], {
+        header: true,
+        dynamicTyping: true,
+        complete: function (results) {
+          console.log(results);
+        },
+      });
+    }
+  }
+
   return (
     <CssVarsProvider>
-      <Button variant="soft">Hello World</Button>
+      <Button variant="soft" component="label">
+        Upload
+        <input type="file" accept="text/csv" hidden onChange={onUploadChange} />
+      </Button>
       <ChartComponent data={{}} axes={{}} />
     </CssVarsProvider>
   );
